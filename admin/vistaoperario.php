2da4df2b75ff7d7f81e5cec418026758c9e801c0
kurt <nietokevink@gmail.com>
2022-04-05 06:20:40 -0500

    registro de pagina

--- admin/vistaoperario.php
+++ admin/vistaoperario.php
@@ -17,7 +17,7 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-
+  
     <!-- Main content -->
     <section class="content">
       <?php
