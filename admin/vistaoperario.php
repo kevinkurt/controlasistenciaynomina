71ee2ce3ce5272cc809c47cb00f5ec7f80a7e674
kurt <nietokevink@gmail.com>
2022-04-05 08:40:46 -0500

    modificacion de vista operario

--- admin/vistaoperario.php
+++ admin/vistaoperario.php
@@ -1,52 +1,69 @@
 <?php include 'includes/session.php'; ?>
-<?php 
-  include '../timezone.php'; 
-  $today = date('Y-m-d');
-  $year = date('Y');
-  if(isset($_GET['year'])){
-    $year = $_GET['year'];
-  }
+<?php
+include '../timezone.php';
+$today = date('Y-m-d');
+$year = date('Y');
+if (isset($_GET['year'])) {
+  $year = $_GET['year'];
+}
 ?>
 <?php include 'includes/header.php'; ?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Operador</title>
+
+</head>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-  	<?php include 'includes/navbarOperador.php'; ?>
-  	<?php include 'includes/menubarOperador.php'; ?>
+    <?php include 'includes/navbarOperador.php'; ?>
+    <?php include 'includes/menubarOperador.php'; ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-  
-    <section class="content-header">
-     
-    <!-- Main content -->
-    <section class="content">
-      
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-          <div class="box-body" style="padding-left: 550px;">
-          
-          <h1>bienvenido</h1>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
 
-        </div>
-      </div>
-    </section>   
+      <section class="content-header">
+
+        <!-- Main content -->
+        <section class="content">
+
+          <div class="row">
+            <div class="col-xs-12">
+              <div class="box">
+                <div class="box-body" style="padding-left: 550px;">
+
+                  <body>
+                    <header>
+                      <section>
+                        <h1>BIENVENIDO</h1>
+                        <span class="logo-lg"><b>NominApp</b></span>
+                      </section>
+                    </header>
+                  </body>
+
+                </div>
+              </div>
+        </section>
 
     </div>
-  	<?php include 'includes/footer.php'; ?>
+    <?php include 'includes/footer.php'; ?>
 
-</div>
-<!-- ./wrapper -->
+  </div>
+  <!-- ./wrapper -->
 
-<!-- Chart Data -->
-<?php
-  $and = 'AND YEAR(date) = '.$year;
+  <!-- Chart Data -->
+  <?php
+  $and = 'AND YEAR(date) = ' . $year;
   $months = array();
   $ontime = array();
   $late = array();
-  for( $m = 1; $m <= 12; $m++ ) {
+  for ($m = 1; $m <= 12; $m++) {
     $sql = "SELECT * FROM attendance WHERE MONTH(date) = '$m' AND status = 1 $and";
     $oquery = $conn->query($sql);
     array_push($ontime, $oquery->num_rows);
@@ -55,7 +72,7 @@
     $lquery = $conn->query($sql);
     array_push($late, $lquery->num_rows);
 
-    $num = str_pad( $m, 2, 0, STR_PAD_LEFT );
+    $num = str_pad($m, 2, 0, STR_PAD_LEFT);
     $month =  date('M', mktime(0, 0, 0, $m, 1));
     array_push($months, $month);
   }
@@ -64,16 +81,17 @@
   $late = json_encode($late);
   $ontime = json_encode($ontime);
 
-?>
-<!-- End Chart Data -->
-<?php include 'includes/scripts.php'; ?>
+  ?>
+  <!-- End Chart Data -->
+  <?php include 'includes/scripts.php'; ?>
 
-<script>
-$(function(){
-  $('#select_year').change(function(){
-    window.location.href = 'home.php?year='+$(this).val();
-  });
-});
-</script>
+  <script>
+    $(function() {
+      $('#select_year').change(function() {
+        window.location.href = 'home.php?year=' + $(this).val();
+      });
+    });
+  </script>
 </body>
+
 </html>
