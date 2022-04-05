a4e063b8b55be825ddf07aa0fa552d894bc5aa50
kurt <nietokevink@gmail.com>
2022-04-05 07:46:33 -0500

    actualizacion de vista

--- admin/vistaoperario.php
+++ admin/vistaoperario.php
@@ -19,65 +19,17 @@
     <!-- Content Header (Page header) -->
   
     <section class="content-header">
-      <h1>
-        Nomina
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
-        <li>Nomina</li>
-      </ol>
-    </section>
-
+     
     <!-- Main content -->
     <section class="content">
       
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
-          <div class="box-body" style="padding-left: 100px;">
-              <div class="chart">
-                <br>
-                <div class="col-md-3 col-sm-6 col-xs-12">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <p>Inventario por terminar</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-alert-circled"></i>
-            </div>
-            <a href="employee.php" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
-         </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">         
-              <p>Inventario productos</p>
-            </div>
-            <div class="icon">
-              <i class="fa fa-bookmark-o"></i>
-            </div>
-            <a href="payroll.php" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-        <div class="small-box bg-yellow">
-            <div class="inner">
-              <p>Modificacion productos</p>
-            </div>
-            <a href="employee.php" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-                <div id="legend" class="text-center"></div>
-                <canvas id="barChart" style="height:350px"></canvas>
-              </div>
-            </div>
-          <div class="row">
-              <br>
-        
-          </div>
+          <div class="box-body" style="padding-left: 550px;">
+          
+          <h1>bienvenido</h1>
+
         </div>
       </div>
     </section>   
